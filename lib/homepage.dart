fe268a23b02b139af0653646c4a293d757efb937
Vedant Barve <barve.vedant@outlook.com>
2022-09-21 18:46:07 +0530

    day7 commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,29 +1,124 @@
-import 'package:flutter/material.dart';
-import 'package:sig_sessions/api_service.dart';
+import 'dart:math';
 
-ApiService api = ApiService();
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    print(theme.brightness);
     return Scaffold(
       appBar: AppBar(),
-      body: FutureBuilder(
-        future: api.sampleFuture(2),
-        builder: (context, rr) {
-          if (rr.hasData) {
-            return const Center(
-              child: Text("Hey I've waited sooo long"),
-            );
-          }
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
+      body: const Center(
+        child: DeviceInfoPlusWidget(),
+      ),
+    );
+  }
+}
+
+class DeviceInfoPlusWidget extends StatefulWidget {
+  const DeviceInfoPlusWidget({super.key});
+
+  @override
+  State<DeviceInfoPlusWidget> createState() => _DeviceInfoPlusWidgetState();
+}
+
+class _DeviceInfoPlusWidgetState extends State<DeviceInfoPlusWidget> {
+  String data = "";
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text("Data :$data"),
+        ElevatedButton(
+          onPressed: () async {
+            DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+            WebBrowserInfo webBrowserInfo = await deviceInfo.webBrowserInfo;
+            setState(() {
+              data = webBrowserInfo.browserName.name;
+            });
+          },
+          child: Text("Press me"),
+        ),
+      ],
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  const CounterWidget({super.key});
+
+  @override
+  State<CounterWidget> createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  @override
+  Widget build(BuildContext context) {
+    int index = 0;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          "The value of counter is : $index",
+          style: const TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: () {
+            setState(() {
+              index += 2;
+              print(index);
+            });
+          },
+          child: const Text("Text Button"),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        //ElevatedButton(
+        //   onPressed: () {},
+        //   child: const Text("Elevated Button"),
+        // )
+      ],
+    );
+  }
+}
+
+class TransformWidget extends StatefulWidget {
+  const TransformWidget({super.key});
+
+  @override
+  State<TransformWidget> createState() => _TransformWidgetState();
+}
+
+class _TransformWidgetState extends State<TransformWidget> {
+  double angle = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: angle,
+      child: GestureDetector(
+        onTap: () {
+          // setState(() {
+
+          // });
+          setState(() {
+            angle += 45;
+            print(angle);
+          });
         },
+        child: const FlutterLogo(
+          size: 250,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sig_sessions/alpha_vantage.dart';
 import 'homepage.dart';
 
 void main() {
@@ -16,7 +15,7 @@ class RootWidget extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark),
-      home: const AlphaView(),
+      home: const HomePage(),
     );
   }
 }
